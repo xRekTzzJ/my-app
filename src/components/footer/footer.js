196cbe7d6824dda9c9a7cd0ac1e98a636e073771
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-18 15:44:39 +0800

    перевел другие компоненты на хуки

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,30 +1,31 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
 import Filters from '../filters';
-export default class Footer extends Component {
-  static propTypes = {
-    doneCounter: PropTypes.number.isRequired,
-    filter: PropTypes.string.isRequired,
-    onFilterClick: PropTypes.func,
-    onClearCompleted: PropTypes.func,
-  };
-  static defaultProps = {
-    onFilterClick: () => {},
-    onClearCompleted: () => {},
-  };
 
-  //Рендер футера
-  render() {
-    const { doneCounter, filter, onFilterClick, onClearCompleted } = this.props;
-    return (
-      <footer className="footer">
-        <span className="todo-count">{`${doneCounter} items left`}</span>
-        <Filters filter={filter} onFilterClick={onFilterClick} />
-        <button className="clear-completed" onClick={onClearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
-}
+const Footer = (props) => {
+  const { doneCounter, filter, onFilterClick, onClearCompleted } = props;
+
+  return (
+    <footer className="footer">
+      <span className="todo-count">{`${doneCounter} items left`}</span>
+      <Filters filter={filter} onFilterClick={onFilterClick} />
+      <button className="clear-completed" onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
+};
+Footer.propTypes = {
+  doneCounter: PropTypes.number.isRequired,
+  filter: PropTypes.string.isRequired,
+  onFilterClick: PropTypes.func,
+  onClearCompleted: PropTypes.func,
+};
+
+Footer.defaultProps = {
+  onFilterClick: () => {},
+  onClearCompleted: () => {},
+};
+
+export default Footer;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
 import NewTaskForm from '../new-task-form';
-export default class Header extends Component {
-  static propTypes = {
-    onCreateElement: PropTypes.func,
-  };
-  static defaultProps = {
-    onCreateElement: () => {},
-  };
 
-  //Рендер хедера
-  render() {
-    const { onCreateElement } = this.props;
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <NewTaskForm onCreateElement={onCreateElement} />
-      </header>
-    );
-  }
-}
+const Header = (props) => {
+  const { onCreateElement } = props;
+
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <NewTaskForm onCreateElement={onCreateElement} />
+    </header>
+  );
+};
+Header.defaultProps = {
+  onCreateElement: () => {},
+};
+
+Header.propTypes = {
+  onCreateElement: PropTypes.func,
+};
+
+export default Header;
